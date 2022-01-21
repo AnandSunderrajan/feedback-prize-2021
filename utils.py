6d16c0fcdc6b35c76c65d1fb818f2621b9a00869
Anand Sunderrajan <36977554+AnandSunderrajan@users.noreply.github.com>
2022-01-20 22:24:37 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,3 +30,54 @@ target_id_map = {
 
 
 id_target_map = {v: k for k, v in target_id_map.items()}
+
+def _prepare_training_data_helper(args, tokenizer, df, train_ids):
+    training_samples = []
+    for idx in tqdm(train_ids):
+        filename = os.path.join(args.input, "train", idx + ".txt")
+        with open(filename, "r") as f:
+            text = f.read()
+
+        encoded_text = tokenizer.encode_plus(
+            text,
+            add_special_tokens=False,
+            return_offsets_mapping=True,
+        )
+        input_ids = encoded_text["input_ids"]
+        input_labels = copy.deepcopy(input_ids)
+        offset_mapping = encoded_text["offset_mapping"]
+
+        for k in range(len(input_labels)):
+            input_labels[k] = "O"
+
+        sample = {
+            "id": idx,
+            "input_ids": input_ids,
+            "text": text,
+            "offset_mapping": offset_mapping,
+        }
+
+        temp_df = df[df["id"] == idx]
+        for _, row in temp_df.iterrows():
+            text_labels = [0] * len(text)
+            discourse_start = int(row["discourse_start"])
+            discourse_end = int(row["discourse_end"])
+            prediction_label = row["discourse_type"]
+            text_labels[discourse_start:discourse_end] = [1] * (discourse_end - discourse_start)
+            target_idx = []
+            for map_idx, (offset1, offset2) in enumerate(encoded_text["offset_mapping"]):
+                if sum(text_labels[offset1:offset2]) > 0:
+                    if len(text[offset1:offset2].split()) > 0:
+                        target_idx.append(map_idx)
+
+            targets_start = target_idx[0]
+            targets_end = target_idx[-1]
+            pred_start = "B-" + prediction_label
+            pred_end = "I-" + prediction_label
+            input_labels[targets_start] = pred_start
+            input_labels[targets_start + 1 : targets_end + 1] = [pred_end] * (targets_end - targets_start)
+
+        sample["input_ids"] = input_ids
+        sample["input_labels"] = input_labels
+        training_samples.append(sample)
+    return training_samples
