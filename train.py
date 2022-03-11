3120db863b988d8729cc92ec5efa6e7c326170a9
Anand Sunderrajan <36977554+AnandSunderrajan@users.noreply.github.com>
2022-03-10 22:24:37 -0600

    dataset class

--- train.py
+++ train.py
@@ -42,3 +42,54 @@ def parse_args():
     parser.add_argument("--optimizer", type=int, default=32, required=False)
     parser.add_argument("--accumulation_steps", type=int, default=1, required=False)
     return parser.parse_args()
+
+class FeedbackDataset:
+    def __init__(self, samples, max_len, tokenizer):
+        self.samples = samples
+        self.max_len = max_len
+        self.tokenizer = tokenizer
+        self.length = len(samples)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        input_ids = self.samples[idx]["input_ids"]
+        input_labels = self.samples[idx]["input_labels"]
+        input_labels = [target_id_map[x] for x in input_labels]
+        other_label_id = target_id_map["O"]
+        padding_label_id = target_id_map["PAD"]
+        # print(input_ids)
+        # print(input_labels)
+
+        # add start token id to the input_ids
+        input_ids = [self.tokenizer.cls_token_id] + input_ids
+        input_labels = [other_label_id] + input_labels
+
+        if len(input_ids) > self.max_len - 1:
+            input_ids = input_ids[: self.max_len - 1]
+            input_labels = input_labels[: self.max_len - 1]
+
+        # add end token id to the input_ids
+        input_ids = input_ids + [self.tokenizer.sep_token_id]
+        input_labels = input_labels + [other_label_id]
+
+        attention_mask = [1] * len(input_ids)
+
+        padding_length = self.max_len - len(input_ids)
+        if padding_length > 0:
+            if self.tokenizer.padding_side == "right":
+                input_ids = input_ids + [self.tokenizer.pad_token_id] * padding_length
+                input_labels = input_labels + [padding_label_id] * padding_length
+                attention_mask = attention_mask + [0] * padding_length
+            else:
+                input_ids = [self.tokenizer.pad_token_id] * padding_length + input_ids
+                input_labels = [padding_label_id] * padding_length + input_labels
+                attention_mask = [0] * padding_length + attention_mask
+
+        return {
+            "ids": torch.tensor(input_ids, dtype=torch.long),
+            "mask": torch.tensor(attention_mask, dtype=torch.long),
+            "targets": torch.tensor(input_labels, dtype=torch.long),
+        }
+
